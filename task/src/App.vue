c3119ea9caa7664dc271c24e8dc432aec1571dfb
MostafaAhmed2310 <mostafa25294.n@gmail.com>
2018-12-11 22:27:38 +0200

    render users list

---
+++ task/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+     <task></task>
+  </div>
+</template>
+<script>
+import task from './components/task.vue'
+
+export default {
+  name: 'app',
+  components: {
+    task
+  }
+
+
+
+}
+</script>
+
+<style>
+*{
+  font-size:7pt;
+  font-family:verdana;
+  text-align:center;
+}
+</style>
---
+++ task/src/components/task.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="task">
+    <div id="divv">
+  <h1> Users List</h1>
+  </div>
+  <br>
+    <table>
+    <tr >
+       <th>Id</th>
+       <th>Name</th>
+       <th>U.name</th>
+       <th>Email</th>
+        <th colspan="6">Address</th>
+       <th>Phone</th>
+       <th>Website</th>
+      <th colspan="3">company</th>
+
+
+    </tr>
+    <tr >
+       <th></th>
+       <th></th>
+       <th></th>
+       <th></th>
+
+        <th>street</th>
+        <th>suite</th>
+        <th>city</th>
+        <th>zipcode</th>
+        <th>latitude</th>
+        <th>lng</th>
+
+       <th></th>
+       <th></th>
+
+      <th>name</th>
+      <th>catchPhrase</th>
+      <th>bs</th>
+
+
+
+    </tr>
+    <tr v-for="user in users">
+      <td>{{user.id}}</td>
+      <td>{{user.name}}</td>
+      <td>{{user.username}}</td>
+      <td>{{user.email}}</td>
+        <td>{{user.address.street}}</td>
+        <td>{{user.address.suite}}</td>
+        <td>{{user.address.city}}</td>
+        <td>{{user.address.zipcode}}</td>
+        <td>{{user.address.geo.lat}}</td>
+        <td>{{user.address.geo.lng}}</td>
+      <td>{{user.phone}}</td>
+      <td>{{user.website}}</td>
+        <td>{{user.company.name}}</td>
+        <td>{{user.company.catchPhrase}}</td>
+        <td>{{user.company.bs}}</td>
+
+    </tr>
+  </table>
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: 'task',
+  props: {
+
+  },
+  data(){
+    return{
+
+        users: []
+
+    }
+  },
+  mounted(){
+        axios.get("https://jsonplaceholder.typicode.com/users")
+        .then(response => {this.users = response.data})
+  }
+}
+</script>
+
+
+<style scoped>
+  table {
+
+ border: 2px solid #822a4a;
+ width: 0%;
+ margin: auto;
+ text-align:center;
+ box-shadow: 3px 3px #a18f81;
+
+}
+
+th, td {
+ text-align: center;
+ padding: 10px;
+
+}
+
+tr{ background-color: #fcfedd;}
+
+th {
+ background-color: #822a4a;
+ color: white;
+}
+
+h1{
+  font-size:24pt;
+  color:#822a4a;
+}
+#divv{
+  background-color:#fcfedd;
+  width:35%;
+  padding:0.1%;
+  margin-left:32%;
+  border-radius:20px;
+  border-bottom: 5px solid #822a4a;
+  box-shadow: 3px 3px #a18f81;
+}
+</style>
